490eb23c27c5e28b7e7c8c9455b3a341ad529221
Menard Josef Morales <menardjosefmorales@Abigails-Mac-mini.local>
2015-04-13 11:10:24 +0800

    update AccountController.php

--- src/Acme/AccountBundle/Controller/AccountController.php
+++ src/Acme/AccountBundle/Controller/AccountController.php
@@ -2,10 +2,7 @@
 // src/Acme/AccountBundle/Controller/AccountController.php
 namespace Acme\AccountBundle\Controller;
 
-
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-//use Acme\AccountBundle\Entity\User;
-
 use Acme\AccountBundle\Form\Type\RegistrationType;
 use Acme\AccountBundle\Form\Model\Registration;
 use Symfony\Component\HttpFoundation\Request;
@@ -28,65 +25,44 @@ class AccountController extends Controller
 
     }
 
+    public function loginAction($activationLink = null)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $user = $em->getRepository('AcmeAccountBundle:User')->findOneBy(
+            array('activationLink' => $activationLink, 'active' => 1)
+        );
+
+        if($user) 
+            $msg = 'You have successfully activated your account.';
+        else 
+            $msg = '';
+
+        $user->setActivationLink(null);
+        $em->flush();
+        
+        return $this->render(
+            'AcmeAccountBundle:Account:login.html.twig',
+            array('msg' => $msg));
+    }
+
     public function createAction(Request $request)
     {
-
-
-//$user = new User();
-/*
-    $form = $this->createFormBuilder($user)
-        ->add('email', 'email')
-        ->add('plainPassword', 'repeated', array(
-           'first_name'  => 'password',
-           'second_name' => 'confirm',
-           'type'        => 'password',
-        ))
-        ->add('firstName','text')
-        ->add('lastName','text')
-        ->add('save', 'submit', array('label' => 'Create user'))
-        ->getForm();
-
-    $form->handleRequest($request);
-
-    if ($form->isValid()) {
-        $data = $form->getData();
-        echo '<pre>';
-        print_r($data);
-        // perform some action, such as saving the task to the database
-        return new Response('sulod created');
-        //return $this->redirectToRoute('task_success');
-    } 
-
-    return $this->render(
-            'AcmeAccountBundle:Account:register.html.twig',
-            array('form' => $form->createView())
-        );
-*/
-        
-
         $em = $this->getDoctrine()->getManager();
 
         $form = $this->createForm(new RegistrationType(), new Registration());
-        //$form = $this->createForm(new UserType(), new User());
 
         $form->handleRequest($request);
-
-        //$initialData = "John Doe, this field is not actually mapped to Product";
-       // $form->get('nonMappedField')->setData($initialData);
 
         if ($form->isValid()) {
 
             $registration = $form->getData();
-            //echo '<pre>';
-            //print_r(Registration.user);exit;
-            $activatioinLink = $registration->getUser()->getEmail();
-            $registration->getUser()->setActivationLink($activatioinLink);
+
+            $activationLink = $registration->getUser()->getEmail();
+            $registration->getUser()->setActivationLink($activationLink);
             $em->persist($registration->getUser());
             $em->flush();
 
-            //$this->sendEmail($registration->getUser());
             return new Response('Account created');
-            //return $this->redirectToRoute('account_create');
         }
 
         return $this->render(
@@ -96,13 +72,13 @@ class AccountController extends Controller
         
     }
 
-    public function activateAction($activatioinLink) {
+    public function activateAction($activationLink) {
         
         $em = $this->getDoctrine()->getManager();
-        $user = $em->getRepository('AcmeAccountBundle:User')->findOneByactivationLink($activatioinLink);
+        $user = $em->getRepository('AcmeAccountBundle:User')->findOneByactivationLink($activationLink);
 
         if (!$user) {
-            throw $this->createNotFoundError(
+            throw $this->createNotFoundException(
                 'Invalid Link'
             );
         }
@@ -110,17 +86,11 @@ class AccountController extends Controller
         $user->setActive(1);
         $em->flush();
 
-        return $this->redirectToRoute('account_register');
-        //return new Response('<html><body>Hello '.$activatioinLink.'!</body></html>');
+        return $this->redirectToRoute('account_login',array('activationLink' => $activationLink));
     }
 
     public function sendEmail($user) 
     {
-       /*echo '<pre>';
-        print_r($user);
-        echo $user->getEmail();
-        echo 'send';
-*/
         $from = 'menardjosef.morales@chromedia.com';
         $to = $user->getEmail();
 
